310846fed91717a9cffee0383f25ca0afe4063a8
setina42 <tina.seg@bluewin.ch>
2018-11-12 14:11:54 +0000

    replace old function

--- abi_geneexpressiondata_dsurqec.sh
+++ abi_geneexpressiondata_dsurqec.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-CompositeTransformUtil --disassemble abi2dsurqec_Composite.h5 Dissasembled
+#CompositeTransformUtil --disassemble abi2dsurqec_Composite.h5 Dissasembled
 
-WarpImageMultiTransform 3 energy.nii.gz warped_withref200.nii.gz 01_Dissasembled_DisplacementFieldTransform.nii.gz 00_Dissasembled_AffineTransform.mat -R dsurqec_200micron_masked.nii.gz
+#WarpImageMultiTransform 3 energy.nii.gz warped_withref200.nii.gz 01_Dissasembled_DisplacementFieldTransform.nii.gz 00_Dissasembled_AffineTransform.mat -R dsurqec_200micron_masked.nii.gz
+#fslorient -copyqform2sform warped_withref200.nii.gz
+
+
+antsApplyTransforms --d 3 -i energy.nii.gz -r dsurqec_200micron_masked.nii -o warped_applyTransform.nii.gz -t abi2dsurqec_Composite.h5
 fslorient -copyqform2sform warped_withref200.nii.gz
 
